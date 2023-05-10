5cdaf108560b272687cbe4e93dac65f47359653e
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2023-05-10 13:33:42 +0200

    swift package file

---
+++ Package.swift
@@ -0,0 +1,22 @@
+import PackageDescription
+
+let package = Package(
+    name: "MycrocastSDK",
+    platforms: [.iOS(.v13)],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "MycrocastSDK",
+            targets: ["MycrocastSDK"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/emqx/CocoaMQTT.git", exact: "2.1.3")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(name: "MycrocastSDK", path: "frameworks/MycrocastSDK.xcframework")
+    ]
+)
