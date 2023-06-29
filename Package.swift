558ae9c61ef8ac02d61cac2e719aa8446031b15f
sven-mycrocast <67374475+sven-mycrocast@users.noreply.github.com>
2023-06-29 14:07:08 +0200

    fixed package

--- Package.swift
+++ Package.swift
@@ -20,5 +20,5 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MycrocastSDK", path: "frameworks/MycrocastSDK.xcframework")
+        .binaryTarget(name: "MycrocastSDK", path: "frameworks/MycrocastSDK.xcframework")]
 )
